382aa5633a22e26fa74f1d1e43ad8849e03e294a
Ayush kumar <aayushkumardubey91@gmail.com>
2024-09-29 13:45:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import "./App.css";
+
+import react from "react";
+
+function App() {
+  const [data, setdata] = useState([]);
+  const [load, setload] = useState(true);
+  const [user, setuser] = useState("iron man");
+
+  useEffect(() => {
+    fetch(`http://www.omdbapi.com/?t=${user}&apikey=b144f9da`)
+      .then((res) => res.json())
+      .then((jsondata) => {
+        setdata(jsondata);
+        setload(false);
+      })
+      .catch((error) => console.error("Fetching API IS showing Error ", error));
+  }, [user]);
+  if (load) {
+    return <h1>Loading....</h1>;
+  }
+
+  const submit = (e) => {
+    setuser(e.target.value);
+    e.preventDefault();
+  };
+
+  return (
+    <>
+    <h1 className="nav">Movie Information </h1>
+      <div className="inp">
+        
+        <input
+          name="user"
+          type="text"
+          value={user}
+          onChange={(e) => setuser(e.target.value)}
+          placeholder="Movie Name"
+        ></input>
+        <button type="submit" onSubmit={submit}>
+          Search
+        </button>
+      </div>
+      <div className="main">
+      <img src={data.Poster} alt="image" className="img"></img>
+      <div className="tx">
+        <h1>Titel : {data.Title}</h1>
+        <h1>Type : {data.Type}</h1>
+        <h1>Genre = {data.Genre}</h1>
+        <h1>Actors = {data.Actors}</h1>
+        <h1>IMDB Rating : {data.imdbRating}</h1>
+        <h1>BoxOffice = {data.BoxOffice}</h1>
+      </div>
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
